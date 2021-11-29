3577fa06906559b75d6a807fabb202ab83d4adbc
Miguel Lima <miguellimaoliver97@gmail.com>
2021-11-29 18:40:59 -0300

    Adicionando soma do array exercicio 2

--- fundamentos/bloco-04-javascript-exercicios/exercicios-arrays.js
+++ fundamentos/bloco-04-javascript-exercicios/exercicios-arrays.js
@@ -1,10 +1,12 @@
 // 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultValores = 0;
 
 for (index = 0; index < numbers.length; index +=1) {
-  
+    resultValores += numbers[index];
     console.log(numbers[index])
 }
 
 // 2
 
+console.log(resultValores)
