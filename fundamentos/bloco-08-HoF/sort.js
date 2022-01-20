00e7588444ba1e5ea9bb77fe6e313f6824e383c4
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-20 19:58:51 -0300

    Sort

---
+++ fundamentos/bloco-08-HoF/sort.js
@@ -0,0 +1,5 @@
+const pizzas = ['Frango com Catupiry', 'Marguerita', 'Gratinada', 'Quatro Queijos'];
+
+console.log(pizzas.sort());
+
+//O método sort() ordena os elementos do próprio array e retorna o array. A ordenação não é necessariamente estável. A ordenação padrão é de acordo com a  pontuação de código unicode.
