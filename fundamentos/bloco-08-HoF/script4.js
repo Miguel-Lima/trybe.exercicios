b1de934a22069e9189dc079a5e1b10e3d771d082
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-20 19:31:53 -0300

    HoF 4, exercicio

---
+++ fundamentos/bloco-08-HoF/script4.js
@@ -0,0 +1,18 @@
+const maquiagens = [
+    {produto: 'Batom', price: 12.20},
+    {produto: 'Lápis de olho', price: 21.50},
+    {produto: 'Rimel', price: 7.99},
+    {produto: 'Skincare', price: 29.30}
+];
+
+// For tradicional
+
+// for (let i = 0; i < maquiagens.length; i += 1) {
+//     console.log(`O produto ${maquiagens[i].produto} custa R$:${maquiagens[i].price}`)
+// }
+
+// ForEach
+
+maquiagens.forEach( (maquiagem) => {
+    console.log(`O produduto ${maquiagem.produto} custa R$ ${maquiagem.price}`)
+})
